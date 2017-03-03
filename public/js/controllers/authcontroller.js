bd62c99501c670b16ed0e94b10498b12f6859746
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-03-02 20:42:07 -0500

    angular is up working on auth

---
+++ public/js/controllers/authcontroller.js
@@ -0,0 +1,4 @@
+function AuthController($http) {
+  var self = this;
+  console.log('WHO YOU!');
+}
--- public/js/router.js
+++ public/js/router.js
@@ -1,12 +1,11 @@
 angular.module('budget-app', ['ui.router'])
-  .config(authInterceptor)
-  .config(MainRouter)
+  // .config(authInterceptor)
+  .config(MainRouter);
 
-  function authInterceptor($httpProvider) {
-    $httpProvider.interceptors.push('AuthInterceptor')
-  }
+  // function authInterceptor($httpProvider) {
+  //   $httpProvider.interceptors.push('AuthInterceptor')
+  // }
 
-  function MainRouter($stateProvider, $urlProvider) {
-
+  function MainRouter($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
   }
