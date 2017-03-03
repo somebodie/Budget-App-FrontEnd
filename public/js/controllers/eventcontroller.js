3700e7abcc0c3eba590b2222ec758908110073ff
acpbm <acpbm0@gmail.com>
2017-03-03 17:22:43 -0500

    Create new event function

--- public/js/controllers/eventcontroller.js
+++ public/js/controllers/eventcontroller.js
@@ -1,3 +1,29 @@
-function EventController() {
+function EventController($http) {
+ var self = this;
+ var server = 'https://polar-retreat-61013.herokuapp.com';
 
+ // var server = 'https://localhost:3000';
+
+
+ function addEvent(eventInfo, currentUser){
+   console.log(eventInfo);
+   console.log(currentUser.id);
+
+   var newEvent = {
+     name: eventInfo.name,
+     budget: eventInfo.budget,
+     user_id:currentUser.id
+   }
+
+   console.log(newEvent);
+
+   $http.post(`${server}/users/${currentUser.id}/events`,
+     newEvent )
+   .then(function(response) {
+     console.log(response);
+     self.newEvent = {};
+   });
+ }
+
+ this.addEvent = addEvent;
 }
