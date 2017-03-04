dfd31932ea40f1c9824353f47baf4bf38ac74a19
acpbm <acpbm0@gmail.com>
2017-03-04 15:16:08 -0500

    removed unused argument

--- public/js/controllers/eventcontroller.js
+++ public/js/controllers/eventcontroller.js
@@ -27,7 +27,7 @@ function EventController($http, $scope, $window) {
    });
  }
 
- function getEvents(currentUser){
+ function getEvents(){
    $http.get(`${server}/users/${$window.localStorage.id}/events`)
    .then(function(response) {
      console.log(response);
@@ -45,6 +45,35 @@ function EventController($http, $scope, $window) {
    });
  }
 
+ function getEventDetails(eventDetails) {
+   console.log(eventDetails);
+   self.name = eventDetails.name;
+   self.budget = parseFloat(eventDetails.budget);
+   console.log(self.name);
+   console.log(typeof(self.budget));
+ }
+
+ function editEvent(userId, eventId){
+   console.log('hit editEvent');
+
+  //  var updatedEvent = {
+  //    name: self.name,
+  //    budget: self.budget,
+  //   //  user_id:currentUser.id
+  //  }
+   //
+  //  $http.put(`${server}/users/${userId}/events/${eventId}`, {name: self.name, budget: self.budget})
+  //  .then(function(response){
+  //    console.log(response);
+  //    self.name = '';
+  //    self.budget = null;
+  //    getEvents();
+  //  });
+
+ }
+
+ self.getEventDetails = getEventDetails;
+ self.editEvent = editEvent;
  self.deleteEvent = deleteEvent;
  self.getEvents = getEvents;
  self.addEvent = addEvent;
