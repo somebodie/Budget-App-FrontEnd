be8dbe579ade62bedf5b5cb14105fde0c207b34a
acpbm <acpbm0@gmail.com>
2017-03-04 19:06:47 -0500

    Add slideshow shell to homepage

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
-   $('.carousel.carousel-slider').carousel({fullWidth: true});
+   $('.carousel-slider').carousel({fullWidth: true});
    $(".button-collapse").sideNav();
    $('.collapsible').collapsible();
 });
